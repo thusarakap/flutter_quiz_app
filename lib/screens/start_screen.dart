5ab097b2c4b6ca0b168f5559df8377cbc07e2ec3
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-09-17 23:17:09 +0530

    Update start_screen.dart

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen(this.startQuiz, {super.key});
@@ -15,12 +16,12 @@ class StartScreen extends StatelessWidget {
           width: 300,
           color: const Color.fromARGB(150, 255, 255, 255),),
           const SizedBox(height: 80),
-          const Text(
+          Text(
             'Learn Flutter the fun way!',
-            style: TextStyle(
+            style: GoogleFonts.lato(
               fontSize: 24,
               fontWeight: FontWeight.bold,
-              color: Color.fromARGB(255, 195, 157, 255),
+              color: const Color.fromARGB(255, 195, 157, 255),
             ),
           ),
           const SizedBox(height: 30),

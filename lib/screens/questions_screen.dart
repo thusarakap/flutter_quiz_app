cb9ef3ded567447bd090dce7688bdfaab5c8919b
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-09-17 20:38:41 +0530

    Update questions_screen.dart

--- lib/screens/questions_screen.dart
+++ lib/screens/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/components/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,6 +11,30 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Question',
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
+          const SizedBox(height: 30),
+          AnswerButton(
+            answerText: 'Answer 1',
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer 2',
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer 3',
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
